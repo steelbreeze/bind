2344045ab998ba53f3e0f02dd8c95b0d824d475b
David Mesquita-Morris <mesmo@steelbreeze.net>
2020-07-04 12:39:53 +0100

    Further simplification and decoupling

--- lib/Observable.d.ts
+++ lib/Observable.d.ts
@@ -12,7 +12,7 @@ export declare class Observable<TState> {
     private observers;
     /**
      * Creates a new instance of the Observer class
-     * @param state The initial underlying state.
+     * @param state The underlying state.
      */
     constructor(state: TState);
     /**
@@ -20,54 +20,17 @@ export declare class Observable<TState> {
      * @param observer A function to call when the state changes; takes the underlying state as a parameter.
      * @param notify Optional flag to control if the observer should be notified immediately with the curent value.
      */
-    attach(observer: Observer<TState>, notify: boolean): Observer<TState>;
+    attach(observer: Observer<TState>, notify?: boolean): Observer<TState>;
     /**
      * Unsubscribe to stop receiving updates when the state changes.
      * @param observer A previously subscribed observer.
      */
     detach(observer: Observer<TState>): void;
     /**
-     * Update the underlying state and notify observers.
+     * Create a callback function that will update the observable state in response to an event
+     * @param f A callback to use to update the state.
      */
-    setState(state: TState): void;
-    /**
-     * Update all observers with the current underlying state.
-     * @private
-     */
+    update(f: (state: TState) => void): void;
     private notify;
-    /**
-     * Bind to state changes by updating the textContent of any HTMLElement.
-     * @param element The HTMLElement to update on state changes.
-     * @param f A function that takes the underlying state and returns the desired text.
-     * @param target The type of binding to perform, one of: [textContent], [innerHTML], or [value].
-     * @param notify Optional flag to control if the element should be updated immediately based on the curent underlying state.
-     */
-    to(element: HTMLElement, target: (element: HTMLElement, f: (state: TState) => string) => Observer<TState>, f: (state: TState) => string, notify?: boolean): Observer<TState>;
-    /**
-     * Receive state changes from value changes of an HTMLInputElement of HTMLSelectElement.
-     * @param element
-     * @param f A function to update the underlying state when the value changes.
-     */
-    from(element: HTMLInputElement | HTMLSelectElement, f: (state: TState, value: string) => void): void;
+    replace(state: TState): void;
 }
-/**
- * Creates an [Observer] that updates an elements textContent upon a state change.
- * @param element The element to update.
- * @param f The function to convert from the observable state to the text content.
- * @returns Returns an [Observer].
- */
-export declare function textContent<TState>(element: HTMLElement, f: (state: TState) => string): (state: TState) => string;
-/**
- * Creates an [Observer] that updates an elements innerHTML upon a state change.
- * @param element The element to update.
- * @param f The function to convert from the observable state to the HTML content.
- * @returns Returns an [Observer].
- */
-export declare function innerHTML<TState>(element: HTMLElement, f: (state: TState) => string): (state: TState) => string;
-/**
- * Creates an [Observer] that updates an elements value upon a state change.
- * @param element The element to update.
- * @param f The function to convert from the observable state to the value.
- * @returns Returns an [Observer].
- */
-export declare function value<TState>(element: HTMLSelectElement | HTMLInputElement, f: (state: TState) => string): (state: TState) => string;
--- src/Observable.ts
+++ src/Observable.ts
@@ -13,7 +13,7 @@ export class Observable<TState> {
 
     /**
      * Creates a new instance of the Observer class
-     * @param state The initial underlying state.
+     * @param state The underlying state.
      */
     public constructor(public state: TState) { }
 
@@ -22,7 +22,7 @@ export class Observable<TState> {
      * @param observer A function to call when the state changes; takes the underlying state as a parameter.
      * @param notify Optional flag to control if the observer should be notified immediately with the curent value.
      */
-    attach(observer: Observer<TState>, notify: boolean): Observer<TState> {
+    attach(observer: Observer<TState>, notify: boolean = true): Observer<TState> {
         // add the new observer
         this.observers.push(observer);
 
@@ -49,80 +49,26 @@ export class Observable<TState> {
     }
 
     /**
-     * Update the underlying state and notify observers.
+     * Create a callback function that will update the observable state in response to an event
+     * @param f A callback to use to update the state.
      */
-    public setState(state: TState): void {
-        // update the new state
-        this.state = state;
+    public update(f: (state: TState) => void): void {
+        // update the state
+        f(this.state);
 
-        // notify observers
         this.notify();
     }
 
-    /**
-     * Update all observers with the current underlying state.
-     * @private
-     */
-    private notify(): void {
+    private notify(): void{
         // update the observers
         for (const observer of this.observers) {
             observer(this.state);
         }
     }
 
-    /**
-     * Bind to state changes by updating the textContent of any HTMLElement.
-     * @param element The HTMLElement to update on state changes.
-     * @param f A function that takes the underlying state and returns the desired text.
-     * @param target The type of binding to perform, one of: [textContent], [innerHTML], or [value].
-     * @param notify Optional flag to control if the element should be updated immediately based on the curent underlying state.
-     */
-    to(element: HTMLElement, target: (element: HTMLElement, f: (state: TState) => string) => Observer<TState>, f: (state: TState) => string, notify: boolean = true): Observer<TState> {
-        return this.attach(target(element, f), notify);
+    public replace(state: TState) {
+        this.state = state;
+
+        this.notify();
     }
-
-    /**
-     * Receive state changes from value changes of an HTMLInputElement of HTMLSelectElement.
-     * @param element 
-     * @param f A function to update the underlying state when the value changes.
-     */
-    from(element: HTMLInputElement | HTMLSelectElement, f: (state: TState, value: string) => void): void {
-        element.onchange = () => {
-            // apply the new value to the observable state
-            f(this.state, element.value);
-
-            // notify the observers
-            this.notify();
-        };
-    }
-}
-
-/**
- * Creates an [Observer] that updates an elements textContent upon a state change.
- * @param element The element to update.
- * @param f The function to convert from the observable state to the text content.
- * @returns Returns an [Observer].
- */
-export function textContent<TState>(element: HTMLElement, f: (state: TState) => string) {
-    return (state: TState) => element.textContent = f(state);
-}
-
-/**
- * Creates an [Observer] that updates an elements innerHTML upon a state change.
- * @param element The element to update.
- * @param f The function to convert from the observable state to the HTML content.
- * @returns Returns an [Observer].
- */
-export function innerHTML<TState>(element: HTMLElement, f: (state: TState) => string) {
-    return (state: TState) => element.innerHTML = f(state);
-}
-
-/**
- * Creates an [Observer] that updates an elements value upon a state change.
- * @param element The element to update.
- * @param f The function to convert from the observable state to the value.
- * @returns Returns an [Observer].
- */
-export function value<TState>(element: HTMLSelectElement | HTMLInputElement, f: (state: TState) => string) {
-    return (state: TState) => element.value = f(state);
 }
